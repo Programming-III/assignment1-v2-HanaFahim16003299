5dc2101c326b8455e4b6b2d5393ee73a95447acd
HanaFahim16003299 <hfahim5623@gmail.com>
2025-11-11 09:12:45 +0200

    Define Student class with yearLevel and major
    Added yearLevel and major attributes to Student class.

--- Student.h
+++ Student.h
@@ -4,6 +4,18 @@
 #include "Person.h"
 #include <string>
 #using namespace std;
+    private:
+    int yearLevel;
+    string major;
+
+public:
+Student (int yearLevel, string major);
+
+void Student::display();
+
+    
+}
+
 //#write student class here
 
 
