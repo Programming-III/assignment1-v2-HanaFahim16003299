480083015133b40bf5c41f670383d7bbe428cbdd
HanaFahim16003299 <hfahim5623@gmail.com>
2025-11-11 09:19:52 +0200

    Implement Person and Student class structures

--- main.cpp
+++ main.cpp
@@ -9,7 +9,29 @@
 using namespace std;
 
 // ==================== Person Class Implementation =========================
+ class Person {
+private: 
+string name;
+int id;
 
+public: 
+
+Person:: Person (name, id) {
+    this.name = name;
+    this.id = id;
+}
+Person () {
+    name = "";
+    id = 0;
+}
+~Person();
+
+display() {
+    cout << "Name:" << name << endl;
+    cout << "ID" << id << endl;
+}
+
+ }
 
 
 
@@ -20,8 +42,29 @@ using namespace std;
 // ==================== Student Class Implementation ====================
 
 
+class Student {
+    private:
+    int yearLevel;
+    string major;
 
+public:
+Student:: Student (int yearLevel, string major) {
+    this. yearLevel = yearLevel;
+    this.major = major;
+}
+Student:: Student () {
+     yearLevel = 0;
+     major = "";
 
+}
+
+void Student::display() {
+cout<< "Year level:" << cout yearlevel << endl;
+cout << "major:" << major << endl;
+    }
+
+    
+}
 
 
 // ==================== Instructor Class Implementation ====================
